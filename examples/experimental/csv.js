49c33efb9da412e024cc7c646097b65b7e63a5ad
oleiade <theo@crevon.me>
2024-05-23 11:18:08 +0200

    [fixup] move csv example, and instantiate in init context

--- examples/experimental/csv.js
+++ examples/experimental/csv.js
@@ -2,18 +2,18 @@ import { open } from 'k6/experimental/fs'
 import csv from 'k6/experimental/csv'
 
 let file;
+let parser;
 (async function () {
 	file = await open('data.csv');
-})();
-
-export default async function() {
-	let parser = new csv.Parser(file, {
+	parser = new csv.Parser(file, {
 		delimiter: ',',
 		skipFirstLine: true,
 		fromLine: 3,
 		toLine: 13,
 	})
+})();
 
+export default async function() {
 	while (true) {
 		const {done, value} = await parser.next();
 		if (done) {
